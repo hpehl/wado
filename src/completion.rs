df28793bb282403162b4730366fbd308e77635fc
Harald Pehl <harald.pehl@gmail.com>
2025-05-20 19:12:19 +0200

    Fix #5: Add dynamic shell completion for wildfly-versions

--- src/completion.rs
+++ src/completion.rs
@@ -1,8 +1,12 @@
 use wildfly_container_versions::VERSIONS;
 
 pub fn version_completion() -> anyhow::Result<()> {
-    VERSIONS
-        .values()
-        .for_each(|v| println!("{}", v.short_version));
+    VERSIONS.values().for_each(|wfc| {
+        if wfc.version.minor == 0 {
+            println!("{}", wfc.version.major)
+        } else {
+            println!("{}.{}", wfc.version.major, wfc.version.minor)
+        }
+    });
     Ok(())
 }
